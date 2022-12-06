5b2c042a8eb97ed2c6c13914f5c15ca28fffdaac
Gustav Lundgren <Gustav.Lundgren@nexergroup.com>
2022-12-06 11:47:45 +0100

    copy line to system clipboard with ctrl-c

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -12,41 +12,34 @@ vim.g.mapleader = ' '
 --   term_mode = 't',
 --   command_mode = 'c',
 
--- Leader --
+-- Leader
 map('n', '<leader>e', ':Lex<CR>', opts) -- Open netrw with \e.
 map('n', '<leader>x', ':q<CR>', opts) -- Quit with \x.
-
 map('n', '<leader>n', ':bnext<CR>', opts) -- Navigate buffers.
 map('n', '<leader>b', ':bprevious<CR>', opts)
-
-map('n', '<leader>y', '"*yiw', opts) -- Yank/paste into and from system clipboard.
-map('n', '<leader>p', '"*p', opts)
-map('v', '<leader>y', '"*y', opts)
-
 map('n', '<leader>l', ':set nohlsearch!<CR>', opts)
 
--- Normal --
+-- Windows-specific bindings
+if (vim.loop.os_uname().sysname == "Windows_NT") then
+    map('n', '<C-c>', '<S-v>"*y', opts) -- Copy line with Ctrl+C.
+end
+
+-- Normal
 map('n', '<A-h>', '<C-w>h', opts) -- Better window navigation.
 map('n', '<A-j>', '<C-w>j', opts)
 map('n', '<A-k>', '<C-w>k', opts)
 map('n', '<A-l>', '<C-w>l', opts)
-
 map('n', '<F2>', ':set invpaste paste?<CR>', opts) -- Toggle paste mode with F2.
-
 map('n', '<A-Up>', ':resize +2<CR>', opts) -- Resize with arrows.
 map('n', '<A-Down>', ':resize -2<CR>', opts)
 map('n', '<A-Left>', ':vertical resize -2<CR>', opts)
 map('n', '<A-Right>', ':vertical resize +2<CR>', opts)
 
--- Insert
-
 -- Visual
 map('v', '<', '<gv', opts) -- indent, and stay in indent mode
 map('v', '>', '>gv', opts)
-
 map('v', '<A-j>', ':m .+1<CR>==', opts) -- move text up and down
 map('v', '<A-k>', ':m .-2<CR>==', opts)
-
 map("v", "p", '"_dP', opts) -- disable regular visual paste behavior
 
 -- Visual Block
